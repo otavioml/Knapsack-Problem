00ff02657e324bb293951c13bb78b00c6da7fc86
Otavio de Meira Lima <otaviomeira2008@hotmail.com>
2022-02-10 22:35:59 -0300

    Fix in backtracking function

--- backtracking.py
+++ backtracking.py
@@ -8,6 +8,10 @@ def backtracking_knapsack (n, w, weights, values):
     if (n == 0 or w == 0):
         return 0
 
+    if (weights[n-1] > w):
+        return backtracking_knapsack(n-1, w, weights, values)
+
+
 
     return max(values[n-1] + backtracking_knapsack(n-1, w-weights[n-1], weights, values),
                backtracking_knapsack(n-1, w, weights, values))
